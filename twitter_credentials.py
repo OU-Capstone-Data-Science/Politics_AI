a51415b1ce5a8e299b6a8bca26643d678cc0917e
Jacob Duvall <jdaleduvall@gmail.com>
2019-11-27 19:36:56 -0600

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,10 +1,10 @@
 import twitter
 # Variables that contains the user credentials to access Twitter API
 # https://python-twitter.readthedocs.io/en/latest/searching.html
-ACCESS_TOKEN = "1194433553418903552-9irxRAd1xiONsOaYKgRrsgBxOMx6VP"
-ACCESS_TOKEN_SECRET = "jzgQ2qxrY6AFswmxzMmlFC0XLpW7ARVLvxoYfLDWszGf6"
-CONSUMER_KEY = "2QWXSvaSFaq7H3L5H5XGdcKgD"
-CONSUMER_SECRET = "Ac5d6jnarnp6xfd1N5iRWgE76o1ikK1Dt0GBOBsNpvXw9mc82z"
+ACCESS_TOKEN = "Contact jdaleduvall@gmail.com for this sensitive information"
+ACCESS_TOKEN_SECRET = "Contact jdaleduvall@gmail.com for this sensitive information"
+CONSUMER_KEY = "Contact jdaleduvall@gmail.com for this sensitive information"
+CONSUMER_SECRET = "Contact jdaleduvall@gmail.com for this sensitive information"
 
 
 def twitter_connection():
