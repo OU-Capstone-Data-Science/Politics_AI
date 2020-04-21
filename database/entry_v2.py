5ab9f84e3b164bb889bffc7add6f68eee012e30c
David Burris <david.burris@ou.edu>
2020-04-20 22:27:00 -0500

    changed database path

--- database/entry_v2.py
+++ database/entry_v2.py
@@ -20,7 +20,7 @@ def scrape_tweets():
     atoken = "1178665586278174721-Zhm3e9rO8s4HAq7N8jRmjOyuK7QUrj"
     asecret = "3Fwp3vqIwnjJA6v6Fjpahof3szIclfekWm0ACQJY5XjDS"
 
-    conn = sqlite3.connect('database/twitter.db')
+    conn = sqlite3.connect('twitter.db')
     c = conn.cursor()
 
     def create_table():
@@ -58,7 +58,7 @@ def scrape_tweets():
         while True:
             try:
                 # open database connection
-                worker_connection = sqlite3.connect('database/twitter.db', check_same_thread=False)
+                worker_connection = sqlite3.connect('twitter.db', check_same_thread=False)
                 worker_cursor = worker_connection.cursor()
 
                 entry = data_queue.get()
