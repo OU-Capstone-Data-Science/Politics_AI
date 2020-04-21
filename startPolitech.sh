43927c58578f2a8aa1a396cacc8bc0172dee605c
David Burris <david.burris@ou.edu>
2020-04-20 23:48:38 -0500

    added to startPolitech.sh

--- startPolitech.sh
+++ startPolitech.sh
@@ -3,5 +3,6 @@
 DIR="${0%/*}"
 cd $DIR/database
 sudo nohup python3 entry_v2.py > stream_output.log &
+sudo nohup python3 cleanup.py > /dev/null &
 cd ..
-sudo nohup python3 main.py > main_output.log &
+sudo nohup python3 main.py > /dev/null &
