6a37c9e2fe0cd533cc949897fda430c9e3c15a26
David Burris <david.burris@ou.edu>
2020-04-21 20:28:16 -0500

    all streams will dump to /dev/null

--- startPolitech.sh
+++ startPolitech.sh
@@ -2,7 +2,6 @@
 
 DIR="${0%/*}"
 cd $DIR/database
-sudo nohup python3 entry_v2.py > stream_output.log &
-sudo nohup python3 cleanup.py > /dev/null &
+sudo nohup python3 entry_v2.py > /dev/null &
 cd ..
 sudo nohup python3 main.py > /dev/null &
